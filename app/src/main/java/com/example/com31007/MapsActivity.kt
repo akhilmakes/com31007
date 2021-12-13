8ca534b7dddfc0a64c082a38fb382e415e252fe1
Akhil <akhil.makes@gmail.com>
2021-12-13 21:34:00 +0000

    Added location permission request

--- app/src/main/java/com/example/com31007/MapsActivity.kt
+++ app/src/main/java/com/example/com31007/MapsActivity.kt
@@ -1,11 +1,18 @@
 package com.example.com31007
 
+import android.Manifest
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener
+import com.google.android.gms.maps.GoogleMap.OnMyLocationClickListener
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
@@ -13,17 +20,43 @@ import com.google.android.gms.maps.model.MarkerOptions
 import com.example.com31007.databinding.ActivityMapsBinding
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
+
 import pl.aprilapps.easyphotopicker.*
 
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+internal class MapsActivity : AppCompatActivity(), OnMapReadyCallback, OnMyLocationButtonClickListener, OnMyLocationClickListener {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
     private lateinit var easyImage: EasyImage
 
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                // Permission is granted. Continue the action or workflow in your
+                // app.
+
+            } else {
+                // Explain to the user that the feature is unavailable because the
+                // features requires a permission that the user has denied. At the
+                // same time, respect the user's decision. Don't link to system
+                // settings in an effort to convince the user to change their
+                // decision.
+                Snackbar.make(findViewById(R.id.map),
+                    "We need access to your location to provide you with our services",
+                    Snackbar.LENGTH_LONG).show()
+            }
+        }
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        requestPermissionLauncher.launch(
+            Manifest.permission.ACCESS_FINE_LOCATION)
 
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -34,13 +67,18 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
+
+        // Floating Action Button to select an image from the gallery
         val fabNewImage: FloatingActionButton = findViewById(R.id.fab_new_image)
-        fabNewImage.setOnClickListener(View.OnClickListener { //view ->
-//            Snackbar.make(view, "Choose an Image to Upload", Snackbar.LENGTH_LONG)
-//                .setAction("Open Gallery",null).show()
+        fabNewImage.setOnClickListener(View.OnClickListener {
             easyImage.openChooser(this@MapsActivity)
+
+
         })
+
+
     }
+
 
     private fun initEasyImage() {
         easyImage = EasyImage.Builder(this)
@@ -63,9 +101,26 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        mMap.setOnMyLocationButtonClickListener(this)
+        mMap.setOnMyLocationClickListener(this)
+
+
     }
+    override fun onMyLocationButtonClick(): Boolean {
+        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show()
+        // Return false so that we don't consume the event and the default behavior still occurs
+        // (the camera animates to the user's current position).
+        return false
+    }
+
+    override fun onMyLocationClick(location: Location) {
+        Toast.makeText(this, "Current location:\n$location", Toast.LENGTH_LONG).show()
+    }
+
+
+    companion object{
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
+    }
+
+
 }
