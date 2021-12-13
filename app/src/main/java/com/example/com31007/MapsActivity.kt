36e7cb68b51f23eb5e115d03c0fc96da5bf07814
Akhil <akhil.makes@gmail.com>
2021-12-13 17:11:01 +0000

    Added the FAB to open gallery

--- app/src/main/java/com/example/com31007/MapsActivity.kt
+++ app/src/main/java/com/example/com31007/MapsActivity.kt
@@ -2,6 +2,7 @@ package com.example.com31007
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -10,22 +11,44 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.example.com31007.databinding.ActivityMapsBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import pl.aprilapps.easyphotopicker.*
+
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+    private lateinit var easyImage: EasyImage
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initEasyImage()
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        val fabNewImage: FloatingActionButton = findViewById(R.id.fab_new_image)
+        fabNewImage.setOnClickListener(View.OnClickListener { //view ->
+//            Snackbar.make(view, "Choose an Image to Upload", Snackbar.LENGTH_LONG)
+//                .setAction("Open Gallery",null).show()
+            easyImage.openChooser(this@MapsActivity)
+        })
+    }
+
+    private fun initEasyImage() {
+        easyImage = EasyImage.Builder(this)
+            .setChooserTitle("Pick media")
+            .setChooserType(ChooserType.CAMERA_AND_GALLERY)
+            .allowMultiple(true)
+            .setCopyImagesToPublicGalleryFolder(true)
+            .build()
     }
 
     /**
