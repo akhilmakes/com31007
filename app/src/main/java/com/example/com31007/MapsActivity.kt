d27d3bf62d368b67e7a1b633f34d66423fc8db0c
Akhil <akhil.makes@gmail.com>
2021-12-14 18:59:20 +0000

    New Methods for Permission

--- app/src/main/java/com/example/com31007/MapsActivity.kt
+++ app/src/main/java/com/example/com31007/MapsActivity.kt
@@ -1,12 +1,15 @@
 package com.example.com31007
 
 import android.Manifest
+import android.content.pm.PackageManager
 import android.location.Location
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -37,8 +40,6 @@ internal class MapsActivity : AppCompatActivity(), OnMapReadyCallback, OnMyLocat
             if (isGranted) {
                 // Permission is granted. Continue the action or workflow in your
                 // app.
-
-
             } else {
                 // Explain to the user that the feature is unavailable because the
                 // features requires a permission that the user has denied. At the
@@ -46,27 +47,93 @@ internal class MapsActivity : AppCompatActivity(), OnMapReadyCallback, OnMyLocat
                 // settings in an effort to convince the user to change their
                 // decision.
                 Snackbar.make(findViewById(R.id.map),
-                    "We need access to your location to provide you with our services",
+                    "We need access to your location to provide our services",
                     Snackbar.LENGTH_LONG).show()
             }
         }
 
 
+    @RequiresApi(Build.VERSION_CODES.M)
+    fun checkPermissions() {
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                // You can use the API that requires the permission.
+            }
+            shouldShowRequestPermissionRationale("ACCESS_FINE_LOCATION")
+            -> {
+                // In an educational UI, explain to the user why your app requires this
+                // permission for a specific feature to behave as expected. In this UI,
+                // include a "cancel" or "no thanks" button that allows the user to
+                // continue using your app without granting the permission.
+                //showInContextUI(...)
+            }
+            else -> {
+                // You can directly ask for the permission.
+                // The registered ActivityResultCallback gets the result of this request.
+                requestPermissionLauncher.launch(
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            }
+        }
+    }
 
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            PERMISSION_REQUEST_CODE -> {
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() &&
+                            grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    // Permission is granted. Continue the action or workflow
+                    // in your app.
+                    val mapFragment = supportFragmentManager
+                        .findFragmentById(R.id.map) as SupportMapFragment
+                    mapFragment.getMapAsync(this)
+
+                } else {
+                    // Explain to the user that the feature is unavailable because
+                    // the features requires a permission that the user has denied.
+                    // At the same time, respect the user's decision. Don't link to
+                    // system settings in an effort to convince the user to change
+                    // their decision.
+                }
+                return
+            }
+
+            // Add other 'when' lines to check for other
+            // permissions this app might request.
+            else -> {
+                // Ignore all other requests.
+            }
+        }
+    }
+
+
+
+
+
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        requestPermissionLauncher.launch(
-            Manifest.permission.ACCESS_FINE_LOCATION)
+        var locationPermission: String = Manifest.permission.ACCESS_FINE_LOCATION
+
+
 
         binding = ActivityMapsBinding.inflate(layoutInflater)
+        checkPermissions()
+        onRequestPermissionsResult(PERMISSION_REQUEST_CODE,arrayOf(locationPermission), intArrayOf())
         setContentView(binding.root)
         initEasyImage()
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+//        val mapFragment = supportFragmentManager
+//            .findFragmentById(R.id.map) as SupportMapFragment
+//        mapFragment.getMapAsync(this)
 
 
         // Floating Action Button to select an image from the gallery
@@ -79,7 +146,6 @@ internal class MapsActivity : AppCompatActivity(), OnMapReadyCallback, OnMyLocat
 
 
     }
-
 
     private fun initEasyImage() {
         easyImage = EasyImage.Builder(this)
@@ -120,7 +186,7 @@ internal class MapsActivity : AppCompatActivity(), OnMapReadyCallback, OnMyLocat
 
 
     companion object{
-        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
+        private const val  PERMISSION_REQUEST_CODE = 1
     }
 
 
